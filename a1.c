a1aef846753ffd663711464bf714fa6c0c7a242e
adisinha1 <41489233+adisinha1@users.noreply.github.com>
2024-08-28 15:53:08 -0400

    Final Commit

--- a1.c
+++ a1.c
@@ -5,21 +5,21 @@ int main()
     int cents;
     scanf("%d", &cents);
 
-    for (int quarters = 0; quarters <= cents / 25; quarters++)
+    for (int quarters = cents / 25; quarters >= 0; quarters--)
     {
         int remain1 = cents - (quarters * 25);
 
-        for (int dimes = 0; dimes <= remain1 / 10; dimes++)
+        for (int dimes = remain1 / 10; dimes >= 0; dimes--)
         {
             int remain2 = remain1 - (dimes * 10);
 
-            for (int nickels = 0; nickels <= remain2 / 5; nickels++)
+            for (int nickels = remain2 / 5; nickels >= 0; nickels--)
             {
                 int pennies = remain2 - (nickels * 5);
-
                 printf("%d quarter(s), %d dime(s), %d nickel(s), %d pennie(s)\n", quarters, dimes, nickels, pennies);
             }
         }
     }
+
     return 0;
 }
